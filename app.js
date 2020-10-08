348de141d25ef4f10d9085df74b0ebca372cd7fb
Bushra Afsar <50298017+Bushraafsar@users.noreply.github.com>
2020-10-08 07:29:14 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let signup=()=>{
+    let email=document.getElementById("email")
+    let password=document.getElementById("password")
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+
+        .then((result)=>{
+            console.log(result)
+        })
+        .catch(function(error){
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorMessage)
+      
+      })
+    
+}
